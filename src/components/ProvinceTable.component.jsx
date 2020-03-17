0af0106647705442fc8a2072033554a6bc37f6cf
SantySun <sunjixiang.gz@gmail.com>
2020-03-17 13:25:18 +1100

    Change table head text color rendering

--- src/components/ProvinceTable.component.jsx
+++ src/components/ProvinceTable.component.jsx
@@ -4,12 +4,23 @@ import React, { Component } from "react";
 class ProviceTable extends Component {
   render() {
     const data = this.props.data;
+    let bg;
+    let textColor;
+    if (data) {
+      if (data.value > 10000) {bg = "#770000";textColor = "text-white"}
+        else if (data.value > 1000) {bg = "#FF0000"; textColor = "text-white"}
+        else if (data.value > 500) {bg = "#FF4D00"; textColor = "text-white"}
+        else if (data.value > 100) {bg = "#FF9A00"; textColor = "text-dark"}
+        else if (data.value > 10) {bg = "#FFC100"; textColor = "text-dark"}
+        else if (data.value > 0) {bg = "#FFFF00";textColor = "text-dark"}
+        else {bg = "#D3D3D3"; textColor = "text-dark"}
+    }
     // console.log(data);
     return data ? (
       <div className="container mt-5 p-3 shadow rounded-lg border border-warning">
-        <h3>{data.pname}疫情数据</h3>
+        <h3 >{data.pname}疫情数据</h3>
         <table className="table table-striped table-hover table-sm table-responsive-sm">
-          <thead className="bg-warning" style={{ fontWeight: "bold", fontSize: 20 }}>
+          <thead className={textColor? textColor : "text-info"} style={{ fontWeight: "bold", fontSize: 20,  backgroundColor:  bg? bg : "#ffc107"}}>
             <tr>
               <th>行政区</th>
               <th>确诊</th>
